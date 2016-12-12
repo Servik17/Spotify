05ab8093ecdbda21c8e883fc95c78d0134382a9e
Сергей Кондратьев < >
2016-12-13 02:15:37 +0500

    add albumsComponent

--- src/Albums.js
+++ src/Albums.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Albums extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: [],
+            artist: ''
+        };
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
+
+    componentDidMount() {
+        const query = `https://api.spotify.com/v1/artists/${this.props.params.id}/albums`;
+        axios.get(query)
+            .then((response) => {
+                this.setState({
+                    images: response.data.items.map(album => album.images[1]),
+                    artist: response.data.items[0].artists[0].name
+                })
+            });
+    }
+
     render() {
+        
         return (
-            <h1>Albums</h1>
+            <div id="images">
+            
+            </div>
         );
     }
 }
