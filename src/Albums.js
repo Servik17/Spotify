d303713a245b7f70266c9e0eadad2c832895c450
Сергей Кондратьев < >
2016-12-13 03:10:34 +0500

    add styles

--- src/Albums.js
+++ src/Albums.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router';
+
 
 export default class Albums extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            images: [],
-            artist: ''
-        };
-        this.componentDidMount = this.componentDidMount.bind(this);
-    }
-
-    componentDidMount() {
-        const query = `https://api.spotify.com/v1/artists/${this.props.params.id}/albums`;
-        axios.get(query)
+        axios.get(`https://api.spotify.com/v1/artists/${this.props.params.id}/albums`)
             .then((response) => {
                 this.setState({
                     images: response.data.items.map(album => album.images[1]),
                     artist: response.data.items[0].artists[0].name
                 })
             });
+        this.state = {
+            images: [],
+            artist: ''
+        };
     }
 
     render() {
+        const images = this.state.images.map((img, ind) => {
+            return <img src={img.url} key={ind} width="150" height="150" role="presentation" />;
+        });
         
         return (
             <div id="images">
-            
+                <h1>Альбомы { this.state.artist }</h1>
+                {images}
+                <Link to="/"><button>Назад</button></Link>
             </div>
         );
     }
--- src/Artists.js
+++ src/Artists.js
@@ -7,7 +7,7 @@ export default class Artists extends React.Component {
         super(props);
         
         this.state = {
-            search: "",
+            search: '',
             artistsList: undefined,
             err: undefined
         };
@@ -19,36 +19,44 @@ export default class Artists extends React.Component {
     changed(event) {
         this.setState({
             search: event.target.value,
+            err: undefined
         });
     }
     
     submited(event) {
         let query = this.state.search;
-        axios.get('https://api.spotify.com/v1/search', {
-            params: {
-              type: 'artist',
-              q: query
-            }
-        }).then((response) => {
-            if (response.data.artists.items.length === 0) {
-                this.setState({
-                    artistsList: undefined,
-                    err: 'Ничего не найдено'
-                });
-            } else {
-                this.setState({                
-                    artistsList: response.data.artists.items,
-                    err: undefined
-                });
-            }
-        }, (err) => {
-            if (err.status === 400) {
-                this.setState({
-                    artistsList: undefined,
-                    err: 'Неверный запрос'
-                })
-            }
-        });
+
+        if (query.length === 0) {
+            this.setState({
+                err: 'Пустое поле поиска'
+            });
+        } else {
+            axios.get('https://api.spotify.com/v1/search', {
+                params: {
+                type: 'artist',
+                q: query
+                }
+            }).then((response) => {
+                if (response.data.artists.items.length === 0) {
+                    this.setState({
+                        artistsList: undefined,
+                        err: 'Ничего не найдено'
+                    });
+                } else {
+                    this.setState({                
+                        artistsList: response.data.artists.items,
+                        err: undefined
+                    });
+                }
+            }, (err) => {
+                if (err.status === 400) {
+                    this.setState({
+                        artistsList: undefined,
+                        err: 'Неверный запрос'
+                    })
+                }
+            });
+        }
         event.preventDefault();
     };
     
@@ -56,19 +64,25 @@ export default class Artists extends React.Component {
         let artists = <div></div>;
         
         if (this.state.err) {
-            artists = <div>{this.state.err}</div>;
+            artists = <div className="error">{this.state.err}</div>;
         } else if (this.state.artistsList) {
             artists = <ArtistsList artists={this.state.artistsList}/>;
         }
 
         return (
             <div>
-                <form onSubmit={this.submited}>
-                    <label>
-                        Поиск:
-                        <input type="text" name="artist" placeholder="Артист" onChange={this.changed}/>
-                    </label>
-                    <input type="submit" value={`Искать ${this.state.search}`} />
+                <form onSubmit={this.submited} className="form-horizontal">
+                    <div className="form-group">
+                        <label htmlFor="search" className="col-sm-2 control-label">Найти</label>
+                        <div className="col-sm-10">
+                            <input className="form-control" id="search" placeholder="Артист" onChange={this.changed}></input>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-offset-2 col-sm-10">
+                            <button type="submit" className="btn btn-default">{`Искать ${this.state.search}`}</button>
+                        </div>
+                    </div>
                 </form>
                 { artists }
             </div>
